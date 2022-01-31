b21f72971aa4c06896bafec80c4b7b17998e5cb6
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-01-31 22:09:58 +0000

    Optimisation + Asset Test
    Added optimisation suggestions from youtube tutorial in "Attractor.cs" and added basic materials for 3 bodies and skybox

--- VRSS CASE-R/Assets/Scripts/Attractor.cs
+++ VRSS CASE-R/Assets/Scripts/Attractor.cs
@@ -19,14 +19,30 @@ public class Attractor : MonoBehaviour
 
     // Using https://www.youtube.com/watch?v=Ouu3D_VHx9o , Optimisations @7:08
 
-    const float G = 6.674f; //Grav Constant
+    const float G = 66.74f; //Grav Constant
 
     public Rigidbody rb;
 
+    //Below is the optimisiation changes
+    public static List<Attractor> Attractors;
+
+    void OnEnable ()
+    {
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+        Attractors.Add(this);
+    }
+
+    void OnDisable ()
+    {
+        Attractors.Remove(this);
+    }
+
     private void FixedUpdate()
     {
-        Attractor[] attractors = FindObjectsOfType<Attractor>();
-        foreach (Attractor attractor in attractors)
+        //Attractor[] attractors = FindObjectsOfType<Attractor>();
+        // The above code line is removed when adding OnEnable/Disable 
+        foreach (Attractor attractor in Attractors)
         {
             if (attractor != this)
                 Attract(attractor);
@@ -38,6 +54,9 @@ public class Attractor : MonoBehaviour
 
         Vector3 direction = rb.position - rbToAttract.position; // Direction of attraction
         float distance = direction.magnitude; //Sets distance float (length of direction vector)
+
+        if (distance == 0f)
+            return; //Avoids error when a duplicate body occupies same space
 
         float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2); //Magnitude of Force
         Vector3 force = direction.normalized * forceMagnitude; //apply newton force in direction of object
