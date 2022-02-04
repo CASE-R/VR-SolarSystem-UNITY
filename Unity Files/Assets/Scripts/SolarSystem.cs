70e13072c39289c4a6b31c1e0dd215e5a40df975
Bruhmao <46407062+Bruhmao@users.noreply.github.com>
2022-02-04 04:09:30 +0000

    Added orbiting
    Using the same looping method, allowed for celestial to orbit around other celestials by setting an initial velocity according to orbital motion formuale.

--- Unity Files/Assets/Scripts/SolarSystem.cs
+++ Unity Files/Assets/Scripts/SolarSystem.cs
@@ -8,10 +8,30 @@ public class SolarSystem : MonoBehaviour
     public float G = 10f;
     GameObject[] celestials;
 
+    void InitialVelocity()
+    {
+        foreach (GameObject celestial1 in celestials)
+        {
+            foreach (GameObject celestial2 in celestials)
+            {
+                if (!celestial1.Equals(celestial2))
+                {
+                    float m2 = celestial2.GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(celestial1.transform.position, celestial2.transform.position);
+                    celestial1.transform.LookAt(celestial2.transform);
+
+                    celestial1.GetComponent<Rigidbody>().velocity += celestial1.transform.right * Mathf.Sqrt((G * m2) / r);
+                }
+
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         celestials = GameObject.FindGameObjectsWithTag("Celestial");
+        InitialVelocity();
     }
 
     // Update is called once per frame
