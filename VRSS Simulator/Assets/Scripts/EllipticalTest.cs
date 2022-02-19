a8c5d2cedd37fc5989e8beb1d9c01bc9872974cc
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-02-19 02:08:39 +0000

    Established elliptical orbits
    Created exaggerated elliptical orbits of celestial bodies using Vis Viva Equation. Next step is to figure out why these don't mimic the correct eccentricity, possibly have to code in an eccentricity factor?

--- VRSS Simulator/Assets/Scripts/EllipticalTest.cs
+++ VRSS Simulator/Assets/Scripts/EllipticalTest.cs
@@ -15,57 +15,55 @@ public class EllipticalTest : MonoBehaviour
     //Gives visual timers (NOT TO BE CHANGED IN EDITOR)
     [SerializeField] private float timeStart;
     [SerializeField] private float physTimeStart;
-    /*[SerializeField] private float fixedDeltaUnscaledTime;
+    /*
+    [SerializeField] private float fixedDeltaUnscaledTime;
     [SerializeField] private float fixedScaledDeltaTime;
     [SerializeField] private float fixedScaledTime;
     */
 
     public float G = 0.08892541f;
-    public GameObject[] celestials;
+    public GameObject[] celestials; // [Sun, M, V, E, Moo, Mars, J, S, U, N]
     float[] semiMajor = { 38.70974211f, 72.33385473f, 99.99799463f, 0.256955307f, 152.3790425f, 520.3806201f, 957.2594553f, 1916.498215f, 3018.05706f };
     float semiMajorAxis;
 
     //Original calculated on initial startup, changing G mid
     void InitialVelocity()
     {
-        foreach (GameObject celestial1 in celestials)
+        for (int COi = 0; COi < 1; COi++) // Want only Sun-Celestial pairing
         {
-            if (celestial1.name == "Sun")
+            for (int COj = 1; COj < celestials.Length; COj++) // Coupling Celestial-semiMajor
             {
-                foreach (GameObject celestial2 in celestials)
+                if (COi != COj)
                 {
-                    if (!celestial1.Equals(celestial2))
-                    {
-                        return celestial2;
-                    }   
-                    foreach (var axis in semiMajor)
-                        {
-                            semiMajorAxis = axis;
 
-                            float mass2 = celestial2.GetComponent<Rigidbody>().mass;
-                            float r = Vector3.Distance(celestial1.transform.position, celestial2.transform.position);
-                            celestial1.transform.LookAt(celestial2.transform);
+                    semiMajorAxis = semiMajor[COj-1]; // Aligning Body-semiMajor coupling to match correct Celestial
 
-                            celestial1.GetComponent<Rigidbody>().velocity += celestial1.transform.forward * Mathf.Sqrt((G * mass2) * ((2 / r) - (1 / 2 * semiMajorAxis)));
-                            print(celestial1 + " " + celestial2 + " " + axis);
+                    float mass1 = celestials[COi].GetComponent<Rigidbody>().mass;
+                    float r = Vector3.Distance(celestials[COi].transform.position, celestials[COj].transform.position);
+                    celestials[COi].transform.LookAt(celestials[COj].transform);
 
-                            //- (1 / (2*i)) 
-                            // celestial1.GetComponent<Rigidbody>().velocity += celestial1.transform.right * Mathf.Sqrt(G * m2 / r);
-                        }
+                    celestials[COj].GetComponent<Rigidbody>().velocity += celestials[COj].transform.forward * Mathf.Sqrt((G * mass1) * ((2 / r) - (1 / (2 * semiMajorAxis))));
+                    
+                    print(celestials[COi] + " is paired to " + celestials[COj] + " with semiMajorAxis " + semiMajorAxis + " and orbitalVelocity " + celestials[COi].GetComponent<Rigidbody>().velocity.magnitude);
+
+                    //- (1 / (2*i)) 
+                    // celestials.[COi].GetComponent<Rigidbody>().velocity += celestials.[COi].transform.right * Mathf.Sqrt(G * m2 / r);
+
                 }
             }
-            
         }
     }
-                    //Vector3 direction = celestial1.transform.position - celestial2.transform.position;
 
-                    //float semiMajor = celestial2.GetComponent<CelestialProperty>().semiMajorAxis;
 
-                    //celestial1.GetComponent<Rigidbody>().velocity = Vector3.Cross(direction, Vector3.forward).normalized * Mathf.Sqrt(G * mass2 * ((2 / r) - (1 / semiMajor)));
+    //Vector3 direction = celestial1.transform.position - celestial2.transform.position;
 
-                    //Debug.Log("Position Vector is " + direction + "with 'magnitude'" + r);
+    //float semiMajor = celestial2.GetComponent<CelestialProperty>().semiMajorAxis;
 
-                    //Debug.Log("Velocity of " + celestial1.name + "is" + celestial1.GetComponent<Rigidbody>().velocity);
+    //celestial1.GetComponent<Rigidbody>().velocity = Vector3.Cross(direction, Vector3.forward).normalized * Mathf.Sqrt(G * mass2 * ((2 / r) - (1 / semiMajor)));
+
+    //Debug.Log("Position Vector is " + direction + "with 'magnitude'" + r);
+
+    //Debug.Log("Velocity of " + celestial1.name + "is" + celestial1.GetComponent<Rigidbody>().velocity);
 
     // Start is called before the first frame update
     void Start()
@@ -107,9 +105,9 @@ public class EllipticalTest : MonoBehaviour
 
                     float r = Vector3.Distance(celestial1.transform.position, celestial2.transform.position);
 
-                    celestial1.GetComponent<Rigidbody>().AddForce((celestial2.transform.position - celestial1.transform.position).normalized * (G*m1*m2/(r*r)));
+                    celestial1.GetComponent<Rigidbody>().AddForce((celestial2.transform.position - celestial1.transform.position).normalized * (G * m1 * m2 / (r * r)));
 
-                    
+
                 }
 
             }
