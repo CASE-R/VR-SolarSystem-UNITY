38402a4d2191d5e3c86de02446e48a2c3f9b5e0f
Bruhmao <46407062+Bruhmao@users.noreply.github.com>
2022-03-23 17:12:28 +0000

    General UI + visual changes
    - Used trial and error to make the trails more visually appealing.
    - Added all the celestials to the list in the menu.
    - Made the menu scroll faster
    - Added a way to monitor the distance from the sun
    - General velocity display which works for all movement of FreeCam

--- VRSS Rework - QK/Assets/Scripts/DistanceDisplay.cs
+++ VRSS Rework - QK/Assets/Scripts/DistanceDisplay.cs
@@ -11,6 +11,11 @@ public class DistanceDisplay : MonoBehaviour
     float ydistance;
     float zdistance;
 
+    Vector3 lastFramePos;
+    Vector3 thisFramePos;
+
+    float velocity;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +25,20 @@ public class DistanceDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        thisFramePos = freeCam.transform.position;
+
+        velocity = Vector3.Distance(thisFramePos, lastFramePos) / Time.fixedDeltaTime;
+
         xdistance = (sun.transform.position.x - freeCam.transform.position.x)/100;
         ydistance = (sun.transform.position.y - freeCam.transform.position.y)/100;
         zdistance = (sun.transform.position.z - freeCam.transform.position.z)/100;
 
-        gameObject.GetComponent<Text>().text = ("Coordinates from sun:\nX:" + xdistance + " AU\nY:" + ydistance + " AU\nZ:" + zdistance + "AU");
+        gameObject.GetComponent<Text>().text = ("Coordinates from sun:\nX: "
+                                                + xdistance.ToString("n5") + " AU\nY: " 
+                                                + ydistance.ToString("n5") + " AU\nZ: " 
+                                                + zdistance.ToString("n5") + "AU\n"
+                                                + "\nCurrent velocity: " + (velocity * 149.598073 / 100).ToString("n5") + "million km/s");
+
+        lastFramePos = freeCam.transform.position;
     }
 }
