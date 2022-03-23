4f9c4d36cb4ef4fdeb9bb6147480dda757f45b90
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-03-23 00:33:04 +0000

    Repo cleanup + New Folder
    - Could not access working scene in "Rework" folder, so attempted to recreate scene in a new cleaned up folder. Not experience w/ UI to have that working to correctly test deltaTime stuff. Still looking for a solution for this
    
    - Removed old/obsolete project folders with old and outdated assets. This should make the repo a bit cleaner and also quicker to upload/download

---
+++ Virtual Solar System/Assets/Scripts/UpdateTimeScale.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTimeScale : MonoBehaviour
+{
+    public GameObject slider;
+    public GameObject timeInput;
+    private SimulationScript simulation;
+
+    public Dropdown timeUnitMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        simulation = GetComponent<SimulationScript>();
+        updateTimescale();
+        updateTimeUnit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateTimeInput()
+    {
+        timeInput.GetComponent<InputField>().text = slider.GetComponent<Slider>().value.ToString();
+    }
+
+    public void updateSlider()
+    {
+        slider.GetComponent<Slider>().value = float.Parse(timeInput.GetComponent<InputField>().text);
+    }
+
+    public void updateTimeUnit()
+    {
+        if (timeUnitMenu.value == 0)
+        {
+            simulation.timeUnitMultiplier = 1f / (24 * 60 * 60);
+            Time.timeScale = simulation.initialTimeScale * simulation.timeUnitMultiplier;
+            Time.fixedDeltaTime = (float)(Time.timeScale * 0.02);
+        }
+
+        else if (timeUnitMenu.value == 1)
+        {
+            simulation.timeUnitMultiplier = 1;
+            Time.timeScale = simulation.initialTimeScale * simulation.timeUnitMultiplier;
+            //Time.fixedDeltaTime = simulation.initialFixedTimeStep;
+        }
+
+        else if (timeUnitMenu.value == 2)
+        {
+            simulation.timeUnitMultiplier = 1 * 7;
+            Time.timeScale = simulation.initialTimeScale * simulation.timeUnitMultiplier;
+            Time.fixedDeltaTime = (float)(Time.timeScale * 0.02);
+        }
+    }
+
+    public void updateTimescale()
+    {
+        if (slider.GetComponent<Slider>().value == 0)
+        {
+            simulation.initialTimeScale = 0.0000001f;   //if the slider is moved all the way to the left, set it to a very low number to avoid dividing by 0
+        }
+
+        else
+        {
+            simulation.initialTimeScale = slider.GetComponent<Slider>().value;
+        }
+
+        Time.timeScale = simulation.initialTimeScale * simulation.timeUnitMultiplier;
+    }
+}
