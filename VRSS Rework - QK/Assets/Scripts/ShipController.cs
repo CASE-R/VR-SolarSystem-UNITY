ea5c2c440a45cdd249bd6136acc55935f7371db0
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-03-24 12:03:21 +0000

    SpaceShip Rework
    - Added new spaceship model from free unity asset store
    
    - Reworked ShipController.cs script combining code from FreeCam.cs and the tutorial. Gives very basic movement but the speed should be accessible to change on external conditions (timeframe, realism etc)
    
    - Added particle trails to the ship thrusters to give a sense of forward/backward motion. These do not move wrt to other degrees of freedom.

--- VRSS Rework - QK/Assets/Scripts/ShipController.cs
+++ VRSS Rework - QK/Assets/Scripts/ShipController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShipController : MonoBehaviour
+public class ShipController : MonoBehaviour // Tutorial: https://www.youtube.com/watch?v=J6QR4KzNeJU&t=1187s + taken from FreeCam.cs
 {
     public float forwardSpeed = 5f, sideSpeed = 3.5f, hoverSpeed = 3f;
     private float activeForwardSpeed, activeSideSpeed, activeHoverSpeed;
@@ -14,37 +14,128 @@ public class ShipController : MonoBehaviour
     private float rollInput;
     public float rollSpeed = 90f, rollAccel = 3.5f;
 
+    /// <summary>
+    /// Sensitivity for free look.
+    /// </summary>
+    public float freeLookSensitivity = 3f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel.
+    /// </summary>
+    public float zoomSensitivity = 10f;
+
+    /// <summary>
+    /// Amount to zoom the camera when using the mouse wheel (fast mode).
+    /// </summary>
+    public float fastZoomSensitivity = 50f;
+
+    /// <summary>
+    /// Set to true when free looking (on right mouse button).
+    /// </summary>
+    private bool looking = false;
+
+    public ParticleSystem[] partSys;
 
     // Start is called before the first frame update
-    void Start()
+    void OnValidate()
     {
-        screenCenter.x = Screen.width / 2;
-        screenCenter.y = Screen.height / 2;
-
-        Cursor.lockState = CursorLockMode.Confined;
+        partSys = GetComponentsInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lookInput.x = Input.mousePosition.x;
-        lookInput.y = Input.mousePosition.y;
-
-        mouseDistance.x = (lookInput.x - screenCenter.x)/screenCenter.y;
-        mouseDistance.y = (lookInput.y - screenCenter.y)/screenCenter.y;
-
-        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+        
 
         rollInput = Mathf.Lerp(rollInput, Input.GetAxis("Roll"), rollAccel * Time.deltaTime);
-        
-        gameObject.transform.Rotate(-mouseDistance.y * lookRotateSpeed * Time.deltaTime, mouseDistance.x * lookRotateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
-        
+               
         // Take keyboard input for movement
         activeForwardSpeed = Mathf.Lerp(activeForwardSpeed, Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAccel*Time.deltaTime);
         activeSideSpeed = Mathf.Lerp(activeSideSpeed, Input.GetAxisRaw("Horizontal") * sideSpeed, sideAccel*Time.deltaTime);
         activeHoverSpeed = Mathf.Lerp(activeHoverSpeed, Input.GetAxisRaw("Hover") * hoverSpeed, hoverAccel*Time.deltaTime);
 
-        // Apply input as transform
-        transform.position = transform.position + new Vector3(activeSideSpeed, activeHoverSpeed, activeForwardSpeed) * Time.deltaTime;
+        // Apply input as transform; changed from tutorial to follow FreeCam.cs logic
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + (transform.right * activeSideSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + (transform.forward * activeForwardSpeed * Time.deltaTime);
+            //ParticleSystem.MainModule psMain = gameObject.GetComponent<ParticleSystem>().main;
+            foreach (ParticleSystem obj in partSys)
+            {
+                var psMain = obj.main;
+                psMain.startSpeed = -activeForwardSpeed;
+            }
+            
+        }
+        else
+        {
+            foreach (ParticleSystem obj in partSys)
+            {
+                var psMain = obj.main;
+                psMain.startSpeed = 0;
+            }
+        }
+        
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, 0, rollInput * rollSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.PageDown) || Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.PageUp))
+        {
+            transform.position = transform.position + (Vector3.up * activeHoverSpeed * Time.deltaTime);
+        }
+
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+
+            transform.position = transform.position + transform.forward * axis * zoomSensitivity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
+
+    void OnDisable()
+    {
+        StopLooking();
+    }
+
+    /// <summary>
+    /// Enable free looking.
+    /// </summary>
+    public void StartLooking()
+    {
+        looking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    /// <summary>
+    /// Disable free looking.
+    /// </summary>
+    public void StopLooking()
+    {
+        looking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 }
