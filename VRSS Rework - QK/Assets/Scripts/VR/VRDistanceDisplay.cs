9bd4920c8c687c37c884cdeabe7fdfac815aac20
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-04-07 23:38:18 +0100

    VR Script Tweak 1
    - Added VR versions of certain scripts to help the UI update in the VR scene. This also required changing some of the initialised variables in scripts with renaming and some reattaching components in the UI. Now there isn't an error spam in the console regarding UI stuff
    
    May require additional VR duplicates in future. This has a pro of allowing a VR version of the simulation and a seperate PC version

---
+++ VRSS Rework - QK/Assets/Scripts/VR/VRDistanceDisplay.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VRDistanceDisplay : MonoBehaviour
+{
+    public GameObject sun;
+    public GameObject freeCam;
+
+    VRCamSwitch VRCamSwitch;
+    SimulationScript simulation;
+    public GameObject system;
+
+    float xdistance;
+    float ydistance;
+    float zdistance;
+
+    Vector3 lastFramePos;
+    Vector3 thisFramePos;
+
+    float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VRCamSwitch = system.GetComponent<VRCamSwitch>();
+        simulation = system.GetComponent<SimulationScript>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() //use this for any motion related calculation since it takes into account the timeScale
+    {
+        
+        if (VRCamSwitch.celNumber == -1)
+        {
+            thisFramePos = freeCam.transform.position;
+
+            velocity = Vector3.Distance(thisFramePos, lastFramePos) / (0.02f);
+
+            xdistance = (sun.transform.position.x - thisFramePos.x) / 100;
+            ydistance = (sun.transform.position.y - thisFramePos.y) / 100;
+            zdistance = (sun.transform.position.z - thisFramePos.z) / 100;
+
+            gameObject.GetComponent<Text>().text = ("Coordinates from sun:\nX: "
+                                                    + xdistance.ToString("n4") + " AU\nY: "
+                                                    + ydistance.ToString("n4") + " AU\nZ: "
+                                                    + zdistance.ToString("n4") + "AU\n"
+                                                    + "\nCurrent velocity: " + (velocity * 149.598073 / 100).ToString("n3") + "million km/in game second");
+            lastFramePos = freeCam.transform.position;
+        }
+        else
+        {
+            thisFramePos = simulation.celestials[VRCamSwitch.celNumber].transform.position;
+
+            if (thisFramePos != lastFramePos)
+            {
+
+                velocity = Vector3.Distance(thisFramePos, lastFramePos) / (0.02f);
+
+                xdistance = (sun.transform.position.x - thisFramePos.x) / 100;
+                ydistance = (sun.transform.position.y - thisFramePos.y) / 100;
+                zdistance = (sun.transform.position.z - thisFramePos.z) / 100;
+
+                gameObject.GetComponent<Text>().text = ("Coordinates from sun:\nX: "
+                                                        + xdistance.ToString("n4") + " AU\nY: "
+                                                        + ydistance.ToString("n4") + " AU\nZ: "
+                                                        + zdistance.ToString("n4") + "AU\n"
+                                                        + "\nCurrent velocity: " + (velocity * 149.598073 / 100).ToString("n3") + "million km/in game second");
+            }
+            lastFramePos = simulation.celestials[VRCamSwitch.celNumber].transform.position;
+        }
+        
+    }
+}
---
+++ VRSS Rework - QK/Assets/Scripts/VR/VRPlanetProperties.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRPlanetProperties : MonoBehaviour
+{
+    public GameObject properties;
+    VRCamSwitch VRCamSwitch;
+    SimulationScript simulation;
+
+    public InputField massInput;
+    public InputField radiusInput;
+    public InputField velocityInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VRCamSwitch = gameObject.GetComponent<VRCamSwitch>();
+        simulation = gameObject.GetComponent<SimulationScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VRCamSwitch.celNumber >= -1)
+        {
+            properties.SetActive(true);
+        }
+
+        else
+        {
+            properties.SetActive(false);
+        }
+    }
+
+    public void RemovePlanet()
+    {
+        simulation.celestials[VRCamSwitch.celNumber].GetComponent<Rigidbody>().mass = 0;
+        simulation.celestials[VRCamSwitch.celNumber].transform.GetChild(0).gameObject.GetComponent<Renderer>().enabled = false;
+        simulation.celestials[VRCamSwitch.celNumber].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        VRCamSwitch.celNumber = -1;
+    }
+
+    public void ChangeMass()
+    {
+        simulation.celestials[VRCamSwitch.celNumber].GetComponent<Rigidbody>().mass = float.Parse(massInput.text);
+    }
+
+    public void ChangeVelocity()
+    {
+        simulation.celestials[VRCamSwitch.celNumber].GetComponent<Rigidbody>().velocity = simulation.celestials[VRCamSwitch.celNumber].GetComponent<Rigidbody>().velocity.normalized * float.Parse(velocityInput.text);
+    }
+
+    public void ChangeRadius()
+    {
+        float newRadius = float.Parse(radiusInput.text);
+        simulation.celestials[VRCamSwitch.celNumber].transform.GetChild(0).gameObject.transform.localScale = new Vector3 (newRadius, newRadius, newRadius);
+    }
+}
