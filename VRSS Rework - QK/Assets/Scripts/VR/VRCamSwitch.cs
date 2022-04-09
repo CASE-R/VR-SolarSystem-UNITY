9e93c9390ffe24f39faf0a09a81dae143a2e21fa
DarkyyBoi <80785712+CASE-R@users.noreply.github.com>
2022-04-09 12:13:24 +0100

    VR FocusCam View Working!
    - Fixed the "Render Texture" method to view FocusCam on VR UI. The problem was that the viewport of the focusCam had 0 width and height, as well as the tracking not properly updating now that we are in simultaneous focus and free cam

--- VRSS Rework - QK/Assets/Scripts/VR/VRCamSwitch.cs
+++ VRSS Rework - QK/Assets/Scripts/VR/VRCamSwitch.cs
@@ -14,6 +14,7 @@ public class VRCamSwitch : MonoBehaviour
 
     public Vector3 objectPosition;
     public Vector3 objectScale;
+    public Vector3 offset;
 
     public Dropdown celestialMenu;
     SimulationScript simulation;
@@ -33,6 +34,7 @@ public class VRCamSwitch : MonoBehaviour
         celNumber = -1;
         currentCamera = HMDCamera;
         HMDCamera.SetActive(true);
+        focusCamera.SetActive(true);
 
     }
 
@@ -61,37 +63,37 @@ public class VRCamSwitch : MonoBehaviour
             }
         }
 
-        if (gameObject.GetComponent<UpdateTimeScale>().timeUnitMenu.value == 0 && celNumber > -1) // Focus onto celestial[celNumber] for smaller timeframes at faster rate
+        if (gameObject.GetComponent<UpdateTimeScale>().timeUnitMenu.value == 0) // Focus onto celestial[celNumber] for smaller timeframes at faster rate
         {
             UpdateFocusCamera();
         }
 
-        if (celNumber > -1)
-        { // Taken from: https://emmaprats.com/p/how-to-rotate-the-camera-around-an-object-in-unity3d/
-            if (Input.GetMouseButtonDown(1))
-            {
-                previousPosition = currentCamera.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
-            }
-            else if (Input.GetMouseButton(1))
-            {
-                Vector3 newPosition = currentCamera.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
-                Vector3 direction = previousPosition - newPosition;
-                distanceToTarget = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.lossyScale.x * simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x * 2f; // Multiplying by localScale.x allows camera to scale outwards when radius is changed via UI
+        //if (celNumber > -1)
+        //{ // Taken from: https://emmaprats.com/p/how-to-rotate-the-camera-around-an-object-in-unity3d/
+        //    if (Input.GetMouseButtonDown(1))
+        //    {
+        //        previousPosition = currentCamera.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
+        //    }
+        //    else if (Input.GetMouseButton(1))
+        //    {
+        //        Vector3 newPosition = currentCamera.GetComponent<Camera>().ScreenToViewportPoint(Input.mousePosition);
+        //        Vector3 direction = previousPosition - newPosition;
+        //        distanceToTarget = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.lossyScale.x * simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x * 2f; // Multiplying by localScale.x allows camera to scale outwards when radius is changed via UI
 
-                float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
-                float rotationAroundXAxis = direction.y * 180; // camera moves vertically
+        //        float rotationAroundYAxis = -direction.x * 180; // camera moves horizontally
+        //        float rotationAroundXAxis = direction.y * 180; // camera moves vertically
 
-                currentCamera.transform.position = simulation.celestials[celNumber].transform.position;
+        //        currentCamera.transform.position = simulation.celestials[celNumber].transform.position;
 
-                currentCamera.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
-                currentCamera.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
+        //        currentCamera.transform.Rotate(new Vector3(1, 0, 0), rotationAroundXAxis);
+        //        currentCamera.transform.Rotate(new Vector3(0, 1, 0), rotationAroundYAxis, Space.World); // <— This is what makes it work!
 
-                currentCamera.transform.Translate(new Vector3(0, 0, -distanceToTarget));
+        //        currentCamera.transform.Translate(new Vector3(0, 0, -distanceToTarget));
 
-                previousPosition = newPosition;
-            }
+        //        previousPosition = newPosition;
+        //    }
 
-        }
+        //}
 
 
 
@@ -116,41 +118,30 @@ public class VRCamSwitch : MonoBehaviour
 
     public void UpdateFocusCamera()
     {
-        if (celNumber > -1)
+
+        focusCamera.transform.LookAt(gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform);
+
+        objectPosition = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.position;
+        objectScale = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.lossyScale;
+        offset = objectScale * simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x * 2f; // Multiplying by localScale.x allows camera to scale outwards when radius is changed via UI
+
+
+        focusCamera.transform.position = objectPosition + offset;
+
+        if (VRplanetProperties.massInput.isFocused == false)
         {
-            Vector3 offset = 5f * objectScale;
-
-            if (!Input.GetKeyDown(KeyCode.Mouse1))
-            {
-                focusCamera.transform.LookAt(gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform);
-                //currentCamera = focusCamera;
-                //currentCamera.SetActive(true);
-                //HMDCamera.SetActive(false);
-
-                objectPosition = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.position;
-                objectScale = gameObject.GetComponent<SimulationScript>().celestials[celNumber].transform.lossyScale;
-                offset = objectScale * simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x * 2f; // Multiplying by localScale.x allows camera to scale outwards when radius is changed via UI
-            }
-
-            currentCamera.transform.position = objectPosition + offset;
-
-            if (VRplanetProperties.massInput.isFocused == false)
-            {
-                VRplanetProperties.massInput.text = simulation.celestials[celNumber].GetComponent<Rigidbody>().mass.ToString();
-            }
-
-            if (VRplanetProperties.velocityInput.isFocused == false)
-            {
-                VRplanetProperties.velocityInput.text = simulation.celestials[celNumber].GetComponent<Rigidbody>().velocity.magnitude.ToString();
-            }
-
-            if (VRplanetProperties.radiusInput.isFocused == false)
-            {
-                VRplanetProperties.radiusInput.text = (simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x).ToString();
-            }
+            VRplanetProperties.massInput.text = simulation.celestials[celNumber].GetComponent<Rigidbody>().mass.ToString();
         }
 
+        if (VRplanetProperties.velocityInput.isFocused == false)
+        {
+            VRplanetProperties.velocityInput.text = simulation.celestials[celNumber].GetComponent<Rigidbody>().velocity.magnitude.ToString();
+        }
 
+        if (VRplanetProperties.radiusInput.isFocused == false)
+        {
+            VRplanetProperties.radiusInput.text = (simulation.celestials[celNumber].transform.GetChild(0).GetComponent<Transform>().localScale.x).ToString();
+        }
     }
 
 }
